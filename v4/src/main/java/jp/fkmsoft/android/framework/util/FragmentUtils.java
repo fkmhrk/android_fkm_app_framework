2cdd11390650349cadb1c26f74714c73130c41f5
hrk fkm <fkm@fkmsoft.jp>
2014-12-23 15:20:26 +0900

    Add transition parameter

--- v4/src/main/java/jp/fkmsoft/android/framework/util/FragmentUtils.java
+++ v4/src/main/java/jp/fkmsoft/android/framework/util/FragmentUtils.java
@@ -4,8 +4,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 
-import jp.fkmsoft.libs.app_framework.R;
-
 /**
  * Utility class for Fragment operation
  */
@@ -30,8 +28,26 @@ public class FragmentUtils {
      * @param tag The tag for next fragment.
      *            You can find next with this tag by {@link android.support.v4.app.FragmentManager#findFragmentByTag(String)}
      */
-    public static void toNextFragment(FragmentManager manager, int containerId, Fragment next, boolean addBackstack, String tag) {
+    public static void toNextFragment(FragmentManager manager, int containerId, Fragment next,
+                                      boolean addBackstack, String tag) {
+        toNextFragment(manager, containerId, next, FragmentTransaction.TRANSIT_FRAGMENT_OPEN,
+                addBackstack, tag);
+    }
+
+    /**
+     * Replaces with next fragment
+     * @param manager Fragment manager
+     * @param containerId Container id
+     * @param next The Fragment you want to replace with
+     * @param transition Passed to {@link android.support.v4.app.FragmentTransaction#setTransition(int)}
+     * @param addBackstack true if you want to add current fragment to backstack with tag ""
+     * @param tag The tag for next fragment.
+     *            You can find next with this tag by {@link android.support.v4.app.FragmentManager#findFragmentByTag(String)}
+     */
+    public static void toNextFragment(FragmentManager manager, int containerId, Fragment next,
+                                      int transition, boolean addBackstack, String tag) {
         FragmentTransaction transaction = manager.beginTransaction();
+        transaction.setTransition(transition);
         if (addBackstack) {
             transaction.addToBackStack("");
         }
